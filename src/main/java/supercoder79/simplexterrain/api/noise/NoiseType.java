05ea02d94e576d498eae2231e57d82d2741ab38e
SuperCoder7979 <k.pranav@gmail.com>
2019-12-06 20:15:28 -0500

    fix improved value noise

--- src/main/java/supercoder79/simplexterrain/api/noise/NoiseType.java
+++ src/main/java/supercoder79/simplexterrain/api/noise/NoiseType.java
@@ -3,7 +3,6 @@ package supercoder79.simplexterrain.api.noise;
 import supercoder79.simplexterrain.noise.gradient.CubicNoise;
 import supercoder79.simplexterrain.noise.gradient.OpenSimplexNoise;
 import supercoder79.simplexterrain.noise.gradient.PerlinNoise;
-import supercoder79.simplexterrain.noise.value.ImprovedValueNoise;
 import supercoder79.simplexterrain.noise.value.ValueNoise;
 import supercoder79.simplexterrain.noise.worley.WorleyNoise;
 
@@ -12,8 +11,7 @@ public enum NoiseType {
 	WORLEY(WorleyNoise.class),
 	PERLIN(PerlinNoise.class),
 	VALUE(ValueNoise.class),
-	CUBIC(CubicNoise.class),
-	IMPROVED_VALUE(ImprovedValueNoise.class);
+	CUBIC(CubicNoise.class);
 
 	public final Class<? extends Noise> noiseClass;
 
--- src/main/java/supercoder79/simplexterrain/noise/value/ValueNoise.java
+++ src/main/java/supercoder79/simplexterrain/noise/value/ValueNoise.java
@@ -1,99 +1,177 @@
 package supercoder79.simplexterrain.noise.value;
 
-import java.util.Random;
-
+import net.minecraft.util.math.MathHelper;
 import supercoder79.simplexterrain.api.noise.Noise;
-import supercoder79.simplexterrain.api.noise.NoiseImplementation;
 
 /**
  * @author Valoeghese
  */
-public class ValueNoise extends Noise {
-	public ValueNoise(long seed) {
-		super(seed);
-		Random initRand = new Random(seed);
-
-		// Scamble code. Thanks mojang :P
-		for(int index = 0; index < 256; ++index) {
-			int newIndexOffset = initRand.nextInt(256 - index);
-			int oldValue = this.q[index];
-			this.q[index] = this.q[newIndexOffset + index];
-			this.q[newIndexOffset + index] = oldValue;
-		}
-	}
-
-	private int[] q = {
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-			21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-			41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-			61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-			81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
-			101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
-			121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140,
-			141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160,
-			161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180,
-			181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200,
-			201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220,
-			221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240,
-			241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255};
-
-	private static final double ONE_HUNDREDTWENTYEIGHTH = (1.0D / 128.0D);
+public final class ValueNoise extends Noise {
+	private final SimpleRandom rand;
 	private final boolean fade = true;
 
-	@Override
-	public double sample(double x, double y, double z) {
-		return 0;
+	private final double offsetX;
+	private final double offsetY;
+	private final double offsetZ;
+
+	public ValueNoise(long seed) {
+		super(seed);
+		rand = new SimpleRandom(seed);
+		rand.init(0, 0);
+		offsetX = rand.randomDouble();
+		offsetY = rand.randomDouble();
+		offsetZ = rand.randomDouble();
 	}
 
-	@Override
-	public double sample(double x, double z) {
-		return (sampleImpl(x, z) * 2) - 1;
+	private double keyPoint(double x, double y) {
+		return (2 * rand.randomDouble(x, y)) - 1;
 	}
 
-	public double sampleImpl(double x, double z) {
-		int x0 = floor(x);
-		int z0 = floor(z);
-		int x1 = ceil(x);
-		int z1 = ceil(z);
-		x -= x0;
-		z -= z0;
+	private double keyPoint(double x, double y, double z) {
+		return (2 * rand.randomDouble(x, y, z)) - 1;
+	}
+
+	private double sampleImpl(double x, double y) {
+		double xFloor = MathHelper.floor(x);
+		double yFloor = MathHelper.floor(y);
+
+		double localX = x - xFloor;
+		double localY = y - yFloor;
+
 		if (fade) {
-			x = fade(x);
-			z = fade(z);
+			localX = fade(localX);
+			localY = fade(localY);
 		}
 
-		double nw = qVal(x0, z0);
-		double ne = qVal(x1, z0);
-		double sw = qVal(x0, z1);
-		double se = qVal(x1, z1);
+		double NW = keyPoint(xFloor, yFloor + 1);
+		double NE = keyPoint(xFloor + 1, yFloor + 1);
+		double SW = keyPoint(xFloor, yFloor);
+		double SE = keyPoint(xFloor + 1, yFloor);
 
-		return lerp(
-				z,
-				lerp(x, nw, ne),
-				lerp(z, sw, se));
+		return MathHelper.lerp(localY, 
+				MathHelper.lerp(localX, SW, SE),
+				MathHelper.lerp(localX, NW, NE));
 	}
 
-	private double qVal(int x, int z) {
-		int index = (x & 0x7F) + (z & 0x7F);
-		return ((double) q[index] * ONE_HUNDREDTWENTYEIGHTH) - 1.0;
-	}
+	private double sampleImpl(double x, double y, double z) {
+		double xFloor = MathHelper.floor(x);
+		double yFloor = MathHelper.floor(y);
+		double zFloor = MathHelper.floor(z);
 
-	private static int floor(double value) {
-		int i = (int)value;
-		return value < (float)i ? i - 1 : i;
-	}
+		double localX = x - xFloor;
+		double localY = y - yFloor;
+		double localZ = z - zFloor;
 
-	private static int ceil(double value) {
-		int i = (int)value;
-		return value > (double)i ? i + 1 : i;
+		if (fade) {
+			localX = fade(localX);
+			localY = fade(localY);
+			localZ = fade(localZ);
+		}
+
+		double NWLow = keyPoint(xFloor, yFloor + 1, zFloor);
+		double NELow = keyPoint(xFloor + 1, yFloor + 1, zFloor);
+		double SWLow = keyPoint(xFloor, yFloor, zFloor);
+		double SELow = keyPoint(xFloor + 1, yFloor, zFloor);
+
+		double NWHigh = keyPoint(xFloor, yFloor + 1, zFloor + 1);
+		double NEHigh = keyPoint(xFloor + 1, yFloor + 1, zFloor + 1);
+		double SWHigh = keyPoint(xFloor, yFloor, zFloor + 1);
+		double SEHigh = keyPoint(xFloor + 1, yFloor, zFloor + 1);
+
+		return MathHelper.lerp(localZ, 
+				MathHelper.lerp(localY, 
+						MathHelper.lerp(localX, SWLow, SELow),
+						MathHelper.lerp(localX, NWLow, NELow)), 
+				MathHelper.lerp(localY, 
+						MathHelper.lerp(localX, SWHigh, SEHigh),
+						MathHelper.lerp(localX, NWHigh, NEHigh)));
 	}
 
 	private static double fade(double n) {
-		return n * n * (3 - (n * 2));
+		return n * n * n * (n * (n * 6 - 15) + 10);
 	}
 
 	@Override
-	public int implementedFunctions() {
-		return NoiseImplementation.NOISE_2D;
+	public double sample(double x, double y) {
+		return sampleImpl(x + offsetX, y + offsetY);
+	}
+
+	@Override
+	public double sample(double x, double y, double z) {
+		return sampleImpl(x + offsetX, y + offsetY, z + offsetZ);
+	}
+}
+
+class SimpleRandom {
+	private long seed;
+	private long localSeed;
+
+	public SimpleRandom(long seed) {
+		this.seed = seed;
+		this.localSeed = seed;
+	}
+
+	public int random(double x, double y, int bound) {
+		init(x, y);
+
+		int result = (int) ((localSeed >> 24) % bound);
+		if (result < 0) {
+			result += (bound - 1);
+		}
+
+		return result;
+	}
+
+	public int random(double x, double y, double z, int bound) {
+		init(x, y, z);
+
+		int result = (int) ((localSeed >> 24) % bound);
+		if (result < 0) {
+			result += (bound - 1);
+		}
+
+		return result;
+	}
+
+	public double randomDouble(double x, double y) {
+		return (double) this.random(x, y, Integer.MAX_VALUE) / (double) Integer.MAX_VALUE;
+	}
+
+	public double randomDouble(double x, double y, double z) {
+		return (double) this.random(x, y, z, Integer.MAX_VALUE) / (double) Integer.MAX_VALUE;
+	}
+
+	public int random(int bound) {
+		int result = (int) ((localSeed >> 24) % bound);
+		if (result < 0) {
+			result += (bound - 1);
+		}
+
+		localSeed += seed;
+		localSeed *= 3412375462423L * localSeed + 834672456235L;
+
+		return result;
+	}
+
+	public double randomDouble() {
+		return (double) this.random(Integer.MAX_VALUE) / (double) Integer.MAX_VALUE;
+	}
+
+	public void init(double x, double y) {
+		localSeed = seed;
+		localSeed += (x * 72624D);
+		localSeed *= 3412375462423L * localSeed + 834672456235L;
+		localSeed += (y * 8963D);
+		localSeed *= 3412375462423L * localSeed + 834672456235L;
+	}
+
+	public void init(double x, double y, double z) {
+		localSeed = seed;
+		localSeed += (x * 72624D);
+		localSeed *= 3412375462423L * localSeed + 834672456235L;
+		localSeed += (y * 8963D);
+		localSeed *= 3412375462423L * localSeed + 834672456235L;
+		localSeed += (z * 56385D);
+		localSeed *= 3412375462423L * localSeed + 834672456235L;
 	}
 }
