d8da13cc1efb4186dcaaec7861e337dbaaa1cc80
SuperCoder7979 <k.pranav@gmail.com>
2019-12-28 00:55:45 -0500

    typo pt 2

--- src/main/java/supercoder79/simplexterrain/configs/noisemodifiers/PeaksConfigData.java
+++ src/main/java/supercoder79/simplexterrain/configs/noisemodifiers/PeaksConfigData.java
@@ -4,5 +4,5 @@ import supercoder79.simplexterrain.SimplexTerrain;
 
 public class PeaksConfigData {
 	public String version = SimplexTerrain.VERSION;
-	public String comment = "The config for the peaks noise modifier is in the main config for compatibility reasons.";
+	public String comment = "The config for the peaks noise modifier remains in the main config for compatibility reasons.";
 }
