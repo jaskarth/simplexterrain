810367c884797404468cf3600e293882df835abb
SuperCoder7979 <k.pranav@gmail.com>
2020-01-04 03:03:00 -0500

    don't worry guys i have the ability to write

--- src/main/java/supercoder79/simplexterrain/api/SimplexFeatures.java
+++ src/main/java/supercoder79/simplexterrain/api/SimplexFeatures.java
@@ -4,7 +4,9 @@ import supercoder79.simplexterrain.api.feature.FeaturePack;
 import supercoder79.simplexterrain.world.feature.smallvegetation.SimplexFeatureImpl;
 
 /**
- * API for
+ * API for registering new feature packs.
+ *
+ * @author SuperCoder79
  */
 public final class SimplexFeatures {
 	private SimplexFeatures() {
