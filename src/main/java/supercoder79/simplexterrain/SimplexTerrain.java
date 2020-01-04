0569611477a5f462c1814428518dfe060083b960
SuperCoder7979 <k.pranav@gmail.com>
2020-01-03 20:58:38 -0500

    update version number

--- src/main/java/supercoder79/simplexterrain/SimplexTerrain.java
+++ src/main/java/supercoder79/simplexterrain/SimplexTerrain.java
@@ -20,7 +20,7 @@ import supercoder79.simplexterrain.world.gen.SimplexChunkGenerator;
 import supercoder79.simplexterrain.world.gen.WorldGeneratorType;
 
 public class SimplexTerrain implements ModInitializer {
-	public static final String VERSION = "0.5.0";
+	public static final String VERSION = "0.6.0";
 
 	public static WorldGeneratorType WORLDGEN_TYPE;
 
