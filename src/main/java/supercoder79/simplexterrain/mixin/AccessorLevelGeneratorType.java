47021cf5454966950016e5aa6e00062800fb6d1d
SuperCoder7979 <k.pranav@gmail.com>
2020-02-04 09:10:45 -0500

    if you have an error, just suppress it
    tiny potato

--- src/main/java/supercoder79/simplexterrain/mixin/AccessorLevelGeneratorType.java
+++ src/main/java/supercoder79/simplexterrain/mixin/AccessorLevelGeneratorType.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.LevelGeneratorType;
 
 @Mixin(LevelGeneratorType.class)
 public interface AccessorLevelGeneratorType {
+    @SuppressWarnings("PublicStaticMixinMember")
     @Invoker("<init>")
     static LevelGeneratorType create(int id, String name) {
         throw new RuntimeException("f");
