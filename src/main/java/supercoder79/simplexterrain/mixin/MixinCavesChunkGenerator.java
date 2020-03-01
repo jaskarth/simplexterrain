3432d45d919b5fb25c40dfc3a25c5dc6f0b0f038
SuperCoder7979 <k.pranav@gmail.com>
2020-03-01 18:34:53 -0500

    add nether biome modifiers

--- src/main/java/supercoder79/simplexterrain/mixin/MixinCavesChunkGenerator.java
+++ src/main/java/supercoder79/simplexterrain/mixin/MixinCavesChunkGenerator.java
@@ -4,6 +4,8 @@ import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.chunk.CavesChunkGenerator;
@@ -18,10 +20,21 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import supercoder79.simplexterrain.noise.gradient.SimplexStyleNoise;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mixin(CavesChunkGenerator.class)
 public class MixinCavesChunkGenerator extends SurfaceChunkGenerator {
 
     @Shadow @Final private double[] noiseFalloff;
+
+    private static Map<Biome, Double> biomeToExpansivenessMap = new HashMap<>();
+
+    static {
+        biomeToExpansivenessMap.put(Biomes.CRIMSON_FOREST, 1.2);
+        biomeToExpansivenessMap.put(Biomes.WARPED_FOREST, 0.8);
+        biomeToExpansivenessMap.put(Biomes.SOUL_SAND_VALLEY, 0.1);
+    }
 
     private SimplexStyleNoise noise;
     private SimplexStyleNoise noise2;
@@ -55,9 +68,20 @@ public class MixinCavesChunkGenerator extends SurfaceChunkGenerator {
             for (int z = 0; z < 16; z++) {
                 posMutable.setZ(z);
 
+                double expansiveness = 0;
+
+                for (int x1 = -1; x1 <= 1; x1++) {
+                    for (int z1 = -1; z1 <= 1; z1++) {
+                        expansiveness += biomeToExpansivenessMap.getOrDefault(
+                            this.biomeSource.getBiomeForNoiseGen((chunk.getPos().x*16) + (x + x1), 32, (chunk.getPos().z*16) + (z + z1)), 1.0);
+                    }
+                }
+
+                expansiveness /= 9;
+
                 for (int y = 0; y < 127; y++) {
                     posMutable.setY(y);
-                    if (getNoiseAt((chunk.getPos().x*16) + x, y, (chunk.getPos().z*16) + z) > getThreshold((chunk.getPos().x*16) + x, y, (chunk.getPos().z*16) + z)) {
+                    if (getNoiseAt(expansiveness, (chunk.getPos().x*16) + x, y, (chunk.getPos().z*16) + z) > getThreshold((chunk.getPos().x*16) + x, y, (chunk.getPos().z*16) + z)) {
                         chunk.setBlockState(posMutable, Blocks.NETHERRACK.getDefaultState(), false);
                     } else if (y < this.getSeaLevel()) {
                         chunk.setBlockState(posMutable, Blocks.LAVA.getDefaultState(), false);
@@ -67,9 +91,8 @@ public class MixinCavesChunkGenerator extends SurfaceChunkGenerator {
         }
     }
 
-    private double getNoiseAt(int x, int y, int z) {
-//        double coefficient = ((noiseCoefficient.sample(x / 150f, y / 75f, z / 150f) + 1) * 5);
-//        System.out.println(coefficient);
+    private double getNoiseAt(double expansiveness, int x, int y, int z) {
+
         double baseline = noise.sample(x / 70f, y / 35f, z / 70f);
         double addition = noise2.sample(x / 70f, y / 35f, z / 70f);
         double addition2 = lowerResolution.sample(x / 35f, y / 20f, z / 35f);
@@ -77,7 +100,7 @@ public class MixinCavesChunkGenerator extends SurfaceChunkGenerator {
         double verticalNoise = vertical.sample(x / 70f, y / 7.5f, z / 70f);
         baseline += (15 / (float)y); //lower bound
         baseline += (-15 / ((float)(y - 130))); //upper bound
-        return (baseline*0.75) + (addition*0.3) + (addition2*0.25) + (addition3*0.175) + (verticalNoise*0.1);
+        return (baseline*0.6) + (addition*0.3*expansiveness) + (addition2*0.25*expansiveness) + (addition3*0.175*expansiveness) + (verticalNoise*0.1*expansiveness);
     }
 
     private double getThreshold(int x, int y, int z) {
