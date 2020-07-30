5e3c4c5bd63d669eb6f50f4ca9a0f079b1543adf
SuperCoder7979 <k.pranav@gmail.com>
2020-07-29 22:27:40 -0400

    slight clouds change

--- src/main/java/supercoder79/simplexterrain/mixin/client/MixinSkyProperties.java
+++ src/main/java/supercoder79/simplexterrain/mixin/client/MixinSkyProperties.java
@@ -1,5 +1,8 @@
 package supercoder79.simplexterrain.mixin.client;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.SkyProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,11 +10,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import supercoder79.simplexterrain.SimplexTerrain;
 
+@Environment(EnvType.CLIENT)
 @Mixin(SkyProperties.class)
 public class MixinSkyProperties {
 	@Inject(method = "getCloudsHeight", at = @At("HEAD"), cancellable = true)
 	private void getCloudsHeight(CallbackInfoReturnable<Float> info) {
-		if (SimplexTerrain.isSimplexEnabled) {
+
+		// I don't feel like making simplex terrain send over multiplayer data, so.... for now this is hardcoded to singleplayer
+		if (SimplexTerrain.isSimplexEnabled && MinecraftClient.getInstance().isInSingleplayer()) {
 			info.setReturnValue((float) SimplexTerrain.CONFIG.cloudHeight);
 		}
 	}
