139f0af7b723243f0be5da8be492e1acf012f672
SuperCoder7979 <k.pranav@gmail.com>
2020-12-05 17:08:49 -0500

    no more crashing

---
+++ src/main/java/supercoder79/simplexterrain/mixin/BiomeLayerSamplerAccessor.java
@@ -0,0 +1,12 @@
+package supercoder79.simplexterrain.mixin;
+
+import net.minecraft.world.biome.layer.util.CachingLayerSampler;
+import net.minecraft.world.biome.source.BiomeLayerSampler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(BiomeLayerSampler.class)
+public interface BiomeLayerSamplerAccessor {
+    @Accessor
+    CachingLayerSampler getSampler();
+}
--- src/main/java/supercoder79/simplexterrain/world/gen/SimplexBiomeSource.java
+++ src/main/java/supercoder79/simplexterrain/world/gen/SimplexBiomeSource.java
@@ -1,10 +1,9 @@
 package supercoder79.simplexterrain.world.gen;
 
-import com.google.common.collect.ImmutableList;
-
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.util.registry.RegistryLookupCodec;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
@@ -14,6 +13,7 @@ import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.biome.source.VanillaLayeredBiomeSource;
 import supercoder79.simplexterrain.SimplexTerrain;
 import supercoder79.simplexterrain.api.Heightmap;
+import supercoder79.simplexterrain.mixin.BiomeLayerSamplerAccessor;
 import supercoder79.simplexterrain.noise.gradient.OpenSimplexNoise;
 import supercoder79.simplexterrain.world.BiomeData;
 import supercoder79.simplexterrain.world.biomelayers.SimplexBiomeLayers;
@@ -70,7 +70,7 @@ public class SimplexBiomeSource extends BiomeSource {
 	public Biome getBiomeAt(int x, int z, BiomeData data) {
 		int height = data.getHeight();
 
-		Biome lowlands = this.lowlandsSampler.sample(this.biomeRegistry, x, z);
+		Biome lowlands = sample(this.lowlandsSampler, x, z);
 		boolean isSwamp = this.biomeRegistry.getId(lowlands) == BiomeKeys.SWAMP.getValue();
 
 		if (data.isRiver()) {
@@ -89,20 +89,35 @@ public class SimplexBiomeSource extends BiomeSource {
 			return lowlands;
 		}
 
-		if (height < SimplexTerrain.CONFIG.seaLevel - 20) return this.deepOceanSampler.sample(this.biomeRegistry, x, z);
-		if (height < SimplexTerrain.CONFIG.seaLevel - 4) return this.oceanSampler.sample(this.biomeRegistry, x, z);
+		if (height < SimplexTerrain.CONFIG.seaLevel - 20) return sample(this.deepOceanSampler, x, z);
+		if (height < SimplexTerrain.CONFIG.seaLevel - 4) return sample(this.oceanSampler, x, z);
 		if (height < SimplexTerrain.CONFIG.lowlandStartHeight + (beachStartSampler.sample(x / 128.0, z / 128.0) * 4)) {
 			//TODO: unhardcode
 			if (this.biomeRegistry.getId(lowlands) == BiomeKeys.BADLANDS.getValue()) return lowlands;
 			if (this.biomeRegistry.getId(lowlands) == BiomeKeys.BADLANDS_PLATEAU.getValue()) return lowlands;
 			if (isSwamp) return lowlands;
-			return this.beachSampler.sample(this.biomeRegistry, x, z);
+			return sample(this.beachSampler, x, z);
 		}
 
-		if (height < SimplexTerrain.CONFIG.midlandStartHeight) return this.lowlandsSampler.sample(this.biomeRegistry, x, z);
-		if (height < SimplexTerrain.CONFIG.highlandStartHeight) return this.midlandsSampler.sample(this.biomeRegistry, x, z);
-		if (height < SimplexTerrain.CONFIG.toplandStartHeight) return this.highlandsSampler.sample(this.biomeRegistry, x, z);
-		return this.toplandsSampler.sample(this.biomeRegistry, x, z);
+		if (height < SimplexTerrain.CONFIG.midlandStartHeight) return sample(this.lowlandsSampler, x, z);
+		if (height < SimplexTerrain.CONFIG.highlandStartHeight) return sample(this.midlandsSampler, x, z);
+		if (height < SimplexTerrain.CONFIG.toplandStartHeight) return sample(this.highlandsSampler, x, z);
+		return sample(this.toplandsSampler, x, z);
+	}
+
+	private Biome sample(BiomeLayerSampler sampler, int x, int z) {
+		int id = ((BiomeLayerSamplerAccessor)sampler).getSampler().sample(x, z);
+		RegistryKey<Biome> registryKey = BuiltinBiomes.fromRawId(id);
+		if (registryKey == null) {
+			return this.biomeRegistry.get(BuiltinBiomes.fromRawId(0));
+		} else {
+			Biome biome = this.biomeRegistry.get(registryKey);
+			if (biome == null) {
+				return this.biomeRegistry.get(BuiltinBiomes.fromRawId(0));
+			} else {
+				return biome;
+			}
+		}
 	}
 
 	@Override
